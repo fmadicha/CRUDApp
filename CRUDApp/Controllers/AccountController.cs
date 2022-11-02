b43d1737b60193966350902928ca106629c89536
Farirai <fmadicha@gmail.com>
2022-11-02 11:16:23 +0200

    Add project files.

---
+++ CRUDApp/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using CRUDApp.Models;
+using CRUDApp.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _db;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(AppDbContext db, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Register()
+        {
+            //checking if role exist
+            if(!_roleManager.RoleExistsAsync(Utility.Helper.Admin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(Utility.Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Utility.Helper.Doctor));
+                await _roleManager.CreateAsync(new IdentityRole(Utility.Helper.Patient));
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async  Task<IActionResult> Register(RegisterViewModel model)
+        {
+            //Server side validation
+            if (ModelState.IsValid)
+            { 
+                //if isvalid create user
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name= model.Name
+            };
+
+                var result = await _userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    //since acc is created automatically sign in the new user
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+        }
+            return View();
+        }
+    }
+}
---
+++ CRUDApp/Utility/Helper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CRUDApp.Utility
+{
+    public  static class Helper
+    {
+        public static string Admin = "Admin";
+        public static string Patient = "Patient";
+        public static string Doctor = "Doctor";
+
+        //dropdown
+        public static List<SelectListItem> GetRoleForDropdown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value=Helper.Admin,Text=Helper.Admin},
+                new SelectListItem{Value=Helper.Patient,Text=Helper.Patient},
+                new SelectListItem{Value=Helper.Doctor,Text=Helper.Doctor}
+            };
+        }
+
+
+    }
+}
